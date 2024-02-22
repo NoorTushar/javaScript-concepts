473fb2bb05c0efcd8562379e0076c3c28abbbefd
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-22 14:38:43 +0600

    this in normal func and arrow func

--- this.js
+++ this.js
@@ -5,10 +5,24 @@ const person = {
    english: 88,
    math: 95,
    science: 94,
+   prop: this, // global scope // so will be WINDOW
+
+   getName: function () {
+      console.log(this);
+      return this.name; // who is calling the function?
+      // person is calling the getName function
+      // so here this will be PERSON
+   },
+
+   // now let us use arrow function
+
+   getNameArrow: () => {
+      console.log(this);
+      return this.name;
+   },
 };
 
-function showThis() {
-   console.log(this);
-}
+console.log(person.prop); // window object
 
-showThis();
+console.log(person.getName()); // person + Tushar
+console.log(person.getNameArrow());
